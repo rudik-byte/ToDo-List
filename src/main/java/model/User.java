82078da80a7c6feb8b6b11d6ac1e319e3ac03910
Anton Rud <professor.toxa@gmail.com>
2021-05-07 22:22:13 +0300

    Edit User model class

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -3,20 +3,39 @@ package model;
 import controller.ToDoController;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
 public class User {
 
+    private static final String REGEX_FOR_NAME = "[A-Z][a-z]+";
+    private static final String REGEX_FOR_EMAIL = "[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}";
+    private static final String REGEX_FOR_PASSWORD = "(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}";
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @Pattern(regexp = REGEX_FOR_NAME,
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Pattern(regexp = REGEX_FOR_NAME,
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+    @Pattern(regexp = REGEX_FOR_EMAIL,
+            message = "Must be a valid e-mail address: exemple@exemple.com")
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Pattern(regexp = REGEX_FOR_PASSWORD,
+            message = "Must be a minimum 6 charters, at least one letter and one number")
+    @Column(name = "password", nullable = false)
     private String password;
 
     @ManyToOne
@@ -28,8 +47,8 @@ public class User {
 
     @ManyToMany
     @JoinTable(name = "todo_collaborator",
-    joinColumns = @JoinColumn(name="collaborator_id"),
-    inverseJoinColumns= @JoinColumn(name =  "todo_id"))
+            joinColumns = @JoinColumn(name = "collaborator_id"),
+            inverseJoinColumns = @JoinColumn(name = "todo_id"))
     private List<ToDo> otherTodos;
 
     public User() {
