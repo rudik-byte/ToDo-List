dd0dcc09e73cacab8287640ae94f73b2846f9630
Anton Rud <professor.toxa@gmail.com>
2021-05-25 13:47:23 +0300

    Added UserRepoTest class

---
+++ src/test/java/rudik/repository/UserRepositoryTest.java
@@ -0,0 +1,29 @@
+package rudik.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Test
+    public void getUserByExistingEmailTest() {
+        Assertions.assertEquals(4, userRepository.getUserByEmail("victor@gmail.com").getId());
+    }
+
+    @Test
+    public void getUserByNotExistingEmailTest() {
+        Assertions.assertNull(userRepository.getUserByEmail("victoriya@mail.com"));
+    }
+}
